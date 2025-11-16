7d584c05a6d1ed3aa625c670361c8ac2aaebb389
abdullahM541-afk <abdullahmotala541@gmail.com>
2025-11-17 03:36:30 +0700

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+/* Abdullah Motala
+script.js
+11/16/2025*/
+
+function hamburger() {
+  var menu = document.getElementById("menu-links");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
